9c47dd8b3c1bfebf6ad8f2519675a3da4c133d1f
Josiah White <me@josiahwhite.io>
2022-07-19 12:09:12 -0400

    Updated bindings

--- build/get-protoc.sh
+++ build/get-protoc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # requires https://github.com/fullstorydev/grpcurl
-grpcurl -plaintext -protoset-out proto/dish.protoset 192.168.100.1:9200 describe SpaceX.API.Device.Device
+grpcurl -plaintext -protoset-out proto/dish.protoset 127.0.0.1:9200 describe SpaceX.API.Device.Device
--- build/regen.sh
+++ build/regen.sh
@@ -5,10 +5,11 @@ protoc --go_out=pkg/    --go-grpc_out=pkg/  --descriptor_set_in=proto/dish.proto
 protoc --go_out=pkg/    --go-grpc_out=pkg/  --descriptor_set_in=proto/dish.protoset spacex/api/device/command.proto
 protoc --go_out=pkg/    --go-grpc_out=pkg/  --descriptor_set_in=proto/dish.protoset spacex/api/device/common.proto
 protoc --go_out=pkg/    --go-grpc_out=pkg/  --descriptor_set_in=proto/dish.protoset spacex/api/device/dish.proto
+protoc --go_out=pkg/    --go-grpc_out=pkg/  --descriptor_set_in=proto/dish.protoset spacex/api/device/dish_config.proto
 protoc --go_out=pkg/    --go-grpc_out=pkg/  --descriptor_set_in=proto/dish.protoset spacex/api/device/wifi.proto
 protoc --go_out=pkg/    --go-grpc_out=pkg/  --descriptor_set_in=proto/dish.protoset spacex/api/device/wifi_config.proto
 protoc --go_out=pkg/    --go-grpc_out=pkg/  --descriptor_set_in=proto/dish.protoset spacex/api/device/transceiver.proto
 
-find pkg/spacex.com -name "*.go" | xargs sed -i.bak 's|spacex.com/api|github.com/starlink-community/starlink-grpc-go/pkg/spacex.com/api|g'
+find pkg/spacex.com -name "*.go" | xargs sed -i.bak 's|spacex.com/api|github.com/JosiahWhite/starlink-grpc-go/pkg/spacex.com/api|g'
 find pkg/spacex.com -name "*.bak" | xargs rm
 
